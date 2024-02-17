faab2e1837d723d099d5d3c7105fe26dabbff369
brahamzapata1992 <brahamzapata1992@gmail.com>
2024-02-17 15:39:00 -0500

    proyecto base

---
+++ FrondEnd/src/components/body/admin/adminProducts/AdminProducts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminProducts = () => {
+  return (
+    <div>Aqui van todos los productos que tengo para editar y eliminar en una tabla</div>
+  )
+}
+
+export default AdminProducts

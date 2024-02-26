e7bcbe159743ae7e96ee1ca3fcdf3e35956d28c3
brahamzapata1992 <brahamzapata1992@gmail.com>
2024-02-25 21:01:46 -0500

    actualizacion footer

--- FrondEnd/src/components/footer/Footer.jsx
+++ FrondEnd/src/components/footer/Footer.jsx
@@ -4,31 +4,39 @@ import { FaTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { IoIosPin } from "react-icons/io";
+import logo from "../../assets/Header/logo.svg";
 import "./footer.css";
 
 function Footer() {
     return (
         <footer className="container">
          <div className="containeriz"> 
-            <p style={{ color: '#000000', fontWeight: 'bold' }}>LOGO</p>
-            <p style={{ color: '#666666', display: 'flex', alignItems: 'center' }}>
-                <MdOutlineEmail style={{ color: '#666666', marginRight: '5px'}} /> &nbsp; info@musicrent.com
-            </p>
-            <p style={{ color: '#666666'}}><IoIosPin style={{ color: '#666666', marginRight: '5px'}} /> &nbsp; Bogotá. Colombia</p>
-         </div> 
-         <div className="containerder"> 
-            <p style={{ color: '#000000', fontWeight: 'bold' }}>SIGUENOS</p>
-            <div className="socialmedia">
-            <FaFacebookF style={{ color: '#666666', marginRight: '5px'}} />
-            &nbsp;&nbsp;&nbsp;
-            <FaTwitter style={{ color: '#666666', marginRight: '5px'}} />
-            &nbsp;&nbsp;&nbsp;
-            <FaInstagram style={{ color: '#666666', marginRight: '5px' }} />
-            
+            <p className='p2'><img  src={logo} alt="Logo" /></p>
             <p style={{ color: '#666666'}}>&copy; 2024 Copyright.MusicRent</p>
-            </div>
-         </div>    
+            
+         </div> 
+            
         </footer>
     );
 }
 export default Footer;
+
+/* Aquí va debajo de la p */
+/*<p style={{ color: '#666666', display: 'flex', alignItems: 'center' }}>
+                <MdOutlineEmail style={{ color: '#666666', marginRight: '10px'}} />info@musicrent.com
+            </p>*/
+/*<p style={{ color: '#666666'}}><IoIosPin style={{ color: '#666666', marginRight: '10px'}} />Bogotá. Colombia</p>*/
+
+/* aqui va redes sociales */
+/*<div className="containerder"> 
+            <p className='p3' style={{ color: '#000000', fontWeight: 'bold'}}>SIGUENOS</p>
+            <div className="socialmedia">
+            <p><FaFacebookF style={{ color: '#666666', marginRight: '10px'}} />
+            &nbsp;
+               <FaTwitter style={{ color: '#666666', marginRight: '10px'}} />
+            &nbsp;
+               <FaInstagram style={{ color: '#666666', marginRight: '10px' }} />
+            </p>
+            
+            </div>
+         </div>*/ 

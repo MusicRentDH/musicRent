680677d5acc2476fc784fcdb82bee6159708b499
brahamzapata1992 <brahamzapata1992@gmail.com>
2024-02-28 19:07:37 -0500

    eliminando repos viejos

--- FrondEnd/src/components/body/admin/adminProducts/AdminProducts.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AdminProducts = () => {
-  return (
-    <div>Aqui van todos los productos que tengo para editar y eliminar en una tabla</div>
-  )
-}
-
-export default AdminProducts
--- FrondEnd/src/components/publicProducts/barraBusquedaHome/BarraBusquedaHome.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BarraBusquedaHome = () => {
-  return (
-    <div>Hola, soy la barra de busqueda</div>
-  )
-}
-
-export default BarraBusquedaHome
